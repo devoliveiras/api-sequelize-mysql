155846759e11a40ec8670fc67d75759df32f602a
devoliveiras <guilhermeao@outlook.com>
2022-05-13 13:58:52 -0300

    Adicionado filtro para retornar registros entre as datas escolhidas via Query Params

--- api/controllers/TurmaController.js
+++ api/controllers/TurmaController.js
@@ -1,16 +1,36 @@
 const  database = require('../models')
+const sequelize = require('sequelize')
+const Op = sequelize.Op 
 
 class TurmaController {
 
     static async pegaTodasAsTurmas(req, res) {
+        const { data_inicial, data_final } = req.query
+        const where = {}
+
+        //Verifica se foram inseridos parametros Query Parameters
+        data_inicial || data_final ? where.data_inicio = {} : null 
+        //Se foi, faz o filtro entre data inicial e final
+        data_inicial ? where.data_inicio[Op.gte] = data_inicial : null
+        data_final ? where.data_inicio[Op.lte] = data_final : null
+
         try{
-            const todasAsTurmas = await database.Turmas.findAll()
+            const todasAsTurmas = await database.Turmas.findAll({ where })
             return res.status(200).json(todasAsTurmas)
         }
         catch(error){
             return res.status(500).json(error.message)
         }
     }
+
+    // {
+    //     where: {
+    //         data_inicio:  {
+    //             [Op.gte]: data,
+    //             [Op.lte]: data
+    //         }
+    //     }
+    // }
 
     static async pegaUmaTurma(req,res) {
         const { id } = req.params
